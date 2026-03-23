3934f8891a6ac13f57ec4748a168dd09ed03dc5b
gayathrisajith10 <148003926+gayathrisajith10@users.noreply.github.com>
2026-03-23 12:02:20 +0530

    Export supabase client as default export

--- frontend/src/supabaseClient.js
+++ frontend/src/supabaseClient.js
@@ -1,6 +1,9 @@
 import {createClient} from '@supabase/supabase-js'
 
-export const supabase= createClient(
+const supabase= createClient(
     "https://imdtkasjxjhlrftxfbei.supabase.co",
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltZHRrYXNqeGpobHJmdHhmYmVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzMDY2OTQsImV4cCI6MjA4Mzg4MjY5NH0.G6pAUzsc-yrWRcXRBOdOWzsHhbB5-jegXlP9NezhY2A"
 );
+
+
+export default supabase

c0f4a0984424bbb0c13e6eab9aa38bb7eda5d0e7
gayathrisajith10 <148003926+gayathrisajith10@users.noreply.github.com>
2026-03-27 18:51:17 +0530

    Add Supabase client initialization in supabase_client.py

---
+++ backend/supabase_client.py
@@ -0,0 +1,6 @@
+from supabase import create_client, Client
+
+url = "https://imdtkasjxjhlrftxfbei.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImltZHRrYXNqeGpobHJmdHhmYmVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgzMDY2OTQsImV4cCI6MjA4Mzg4MjY5NH0.G6pAUzsc-yrWRcXRBOdOWzsHhbB5-jegXlP9NezhY2A"
+
+supabase: Client = create_client(url, key)

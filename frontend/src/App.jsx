6a3e37d6142ead7ba05423246a75845933c46d2d
gayathrisajith10 <148003926+gayathrisajith10@users.noreply.github.com>
2026-03-29 13:27:36 +0530

    Simplify App component to render only Home
    Removed routing and simplified App component to only render Home.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,7 @@
-import { Routes, Route } from "react-router-dom";
-
-import Home from "./Home";
-import Emergency from "./Emergency";
-import Auth from "./pages/Auth";
-import Features from "./pages/Features";
-import Dashboard from "./pages/Dashboard";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/emergency" element={<Emergency />} />
-      <Route path="/auth" element={<Auth />} />
-      <Route path="/features" element={<Features />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-    </Routes>
-  );
+  return <Home />;
 }
 
 export default App;
